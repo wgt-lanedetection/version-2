3e0a17b64da4369bcd8cf2440d1472680fae4102
wgt-lanedetection <51018875+wgt-lanedetection@users.noreply.github.com>
2019-05-25 15:41:30 +0000

    Add files via upload

---
+++ Kamera Kalibrierung/get20pics.py
@@ -0,0 +1,13 @@
+from picamera import PiCamera
+from time import sleep
+
+
+camera = PiCamera()
+# set resulution
+camera.resolution = (320, 192)
+camera.start_preview()
+
+for i in range(20):
+    sleep(2)
+    camera.capture('/home/pi/lane/calibration/calibration%s.jpg' % i)
+camera.stop_preview()

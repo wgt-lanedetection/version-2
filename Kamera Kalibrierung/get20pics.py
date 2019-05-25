4548957efe67a0b0b7f4576b05061dcea7d70211
wgt-lanedetection <51018875+wgt-lanedetection@users.noreply.github.com>
2019-05-25 17:44:50 +0200

    Update get20pics.py

--- Kamera Kalibrierung/get20pics.py
+++ Kamera Kalibrierung/get20pics.py
@@ -3,11 +3,11 @@ from time import sleep
 
 
 camera = PiCamera()
-# set resulution
+# Größe des Bildes einstellen
 camera.resolution = (320, 192)
 camera.start_preview()
 
 for i in range(20):
-    sleep(2)
+    sleep(2)    #Bilder im Abstand von 2 Mintuen erstellen
     camera.capture('/home/pi/lane/calibration/calibration%s.jpg' % i) # Hier den Pfad eingeben
 camera.stop_preview()
